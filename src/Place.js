2ae7f31edd94bbe8672510df82d7cba77d8263aa
izrael707 <dkingisreal@gmail.com>
2022-11-15 12:44:07 +0100

    fix: padding

--- src/Place.js
+++ src/Place.js
@@ -44,7 +44,7 @@ const Place = () => {
 
 const CardList = () => {
   return (
-    <div class="row px-2 py-5 px-lg-4">
+    <div class="row px-2 py-5 px-lg-5">
       {cardData2.map((info, id) => (
         <div className="col-12 col-md-6 col-lg-3 p-2" key={id}>
           <Card title={info.title} price={info.price} distance={info.distance} time={info.time} logo={info.logo} />
